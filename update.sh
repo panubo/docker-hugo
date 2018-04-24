ab165694cc8599a14b4daec40deedc5d6df34f18
Andrew Cutler <macropin@gmail.com>
2018-04-24 12:36:19 +1000

    Add osx compat

--- update.sh
+++ update.sh
@@ -4,6 +4,8 @@
 FILE_SUFIX="Linux-64bit.tar.gz"
 LATEST_URL="https://api.github.com/repos/gohugoio/hugo/releases/latest"
 
+[[ "$(uname -s)" == "Darwin" ]] && osx_compat="g" || true
+
 json="$(curl -sSf "${LATEST_URL}")"
 
 latest="$(echo "${json}" | jq -r .tag_name)"
@@ -17,7 +19,7 @@ checksum="$(curl -sSfL "${checksums_url}" | sed -E -e "/${FILE_SUFIX}/!d" -e 's/
 
 echo "Checksum: ${checksum}"
 
-sed -i -E -e "s/HUGO_VERSION=[0-9\\.]+/HUGO_VERSION=${latest#v}/" -e "s/HUGO_CHECKSUM=[0-9a-f]+/HUGO_CHECKSUM=${checksum}/" Dockerfile
+${osx_compat}sed -i -E -e "s/HUGO_VERSION=[0-9\\.]+/HUGO_VERSION=${latest#v}/" -e "s/HUGO_CHECKSUM=[0-9a-f]+/HUGO_CHECKSUM=${checksum}/" Dockerfile
 
 # Get the github assets
 # echo "${json}" | jq '.assets[] | select(.browser_download_url | contains("Linux-64bit.tar.gz"))'
